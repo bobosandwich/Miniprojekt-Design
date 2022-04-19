609122df58d705343afc2be9480f011036cd064b
PentaTraxUCN <10434665@ucn.dk>
2022-04-19 12:23:52 +0200

    package renaming

---
+++ Miniprojekt Design Source/model/Track.java
@@ -0,0 +1,28 @@
+package model;
+
+/**
+ * Music track
+ * 
+ * @author (fen, mars) 
+ * @version (2003.10.27, 2009.29.09)
+ */
+public class Track{
+    private int length;//time in sec.
+    private String title;
+    
+    /**
+     * Constructor for objects of class Track
+     */
+    public Track(String tit, int len)    {
+        this.length= len;
+        title= tit;
+    }
+
+    public int getLength(){
+        return length;
+    }
+    
+    public String getTitle(){
+        return title;
+    }
+}
