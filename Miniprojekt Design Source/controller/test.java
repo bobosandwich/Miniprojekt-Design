ef4d09b1a5b5a37e1a9be09f61ce9312b2b2b2b4
KenniOBirch <102733195+KenniOBirch@users.noreply.github.com>
2022-04-21 14:14:35 +0200

    Update test.java

--- Miniprojekt Design Source/controller/test.java
+++ Miniprojekt Design Source/controller/test.java
@@ -41,18 +41,18 @@ public class test
     public void setUp()
     {
         friendCo1 = new controller.FriendController();
-        friendCo1.addFriend("Julian", "Havnegade", "9000", "Ålborg", "25263395");
+        friendCo1.addFriend("Julian", "Havnegade", "9000", "lborg", "25263395");
         friendCo1.addFriend("Kenni", "Vestergade 22", "7500", "Skive", "55609980");
         friendCo1.addFriend("Philip", "Strandvejen", "5000", "Ribe", "22445590");
         copyCont1 = new controller.CopyController();
         copyCont1.addCopy("Metallica", 11, "Januar", 100);
-        copyCont1.addCopy("Michael Jackson", 22, "igår", 200);
-        copyCont1.addCopy("Spice Girls", 33, "igår", 300);
+        copyCont1.addCopy("Michael Jackson", 22, "igr", 200);
+        copyCont1.addCopy("Spice Girls", 33, "igr", 300);
         loanCont1 = new controller.LoanController();
-        loanCont1.addCopyToLoan(11);
-        loanCont1.addFriendToLoan("Kenni");
-        loanCont1.addLoan("11", "Januar", "Februar");
-        loanCont1.getReturnedDate("11");
+        //loanCont1.addCopyToLoan(11);
+        //loanCont1.addFriendToLoan("Kenni");
+        //loanCont1.addLoan("11", "Januar", "Februar");
+        //loanCont1.getReturnedDate("11");
     }
 
     /**
